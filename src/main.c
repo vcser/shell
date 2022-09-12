9aa4c3dfc3cf7efd73880f83202f707f5b259a9c
crmunozb <113199318+crmunozb@users.noreply.github.com>
2022-09-12 02:36:40 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -13,8 +13,31 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void shell_parse() {
-
+void shell_parse(char s[], char ** parseS) {
+    char tmpW[s_len];
+    int tmpIndex = 0;
+    int parseIndex = 0;
+    for(int i = 0; i<strlen(s);i++){
+        if(s[i] != '\n' && s[i] != ' '){
+            tmpW[tmpIndex] = s[i];
+            tmpIndex++;
+        }
+        else if((s[i] == '\n' || s[i] == ' ') && tmpIndex > 0){
+            tmpW[tmpIndex] = '\0';
+            parseS[parseIndex] = (char *) malloc((strlen(tmpW))+1;
+            if(parseS[parseIndex] == NULL){
+                freeArr(parseS);
+                fprint(stderr,"Malloc Failed");
+                exit(1);
+            }
+                                                    
+              strcpy(parseS[parseIndex], tmpW);
+              parseIndex++;
+              tmpIndex = 0;                                   
+                                                                                            
+             }
+     }
+                                                 
 }
 
 void shell_getline() {
